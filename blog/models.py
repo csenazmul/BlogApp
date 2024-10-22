7796226076f53bb40c3ef69d6a6c48750a9f3a31
csenazmul <csenazmul77@gmail.com>
2024-10-22 09:09:08 +0600

    update blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Writer
+
+    def __str__(self):
+        return self.title
