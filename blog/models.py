3cd729e2b91c58b2d2691ebfec9df2d6eaa8bd87
csenazmul <csenazmul77@gmail.com>
2024-10-25 17:52:58 +0600

    update admin, models

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.conf import settings
 
+# Django 5 the database servers current date
+# from django.db.models.functions import Now  
 # Create your models here.
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Writer
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)  # Writer
+    publish = models.DateTimeField(default=timezone.now)
+    # publish = models.DateTimeField(db_default=Now()) # Django 5 the database servers current date
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
 
     def __str__(self):
         return self.title
