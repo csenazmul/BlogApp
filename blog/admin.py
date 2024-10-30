06e38fe70587a5c89317a584fb100a17d91bbba4
csenazmul <csenazmul77@gmail.com>
2024-10-30 23:10:58 +0600

    Update admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
 
-admin.site.register(Post)
+from blog import models as blog_models
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status')
+    search_fields = ('title', 'body')
+
+admin.site.register(blog_models.Post, PostAdmin)
+
+# from .models import Post
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'created_at')
+#     search_fields = ('title', 'content')
+
+# admin.site.register(Post)
+
